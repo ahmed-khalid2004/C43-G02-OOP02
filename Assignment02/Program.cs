d05d89209be613b18ca51e20f416ba5800209507
ahmed-khalid2004 <ahmadhamadakhalid16@gmail.com>
2025-01-08 20:17:04 +0200

    Add project files.

---
+++ Assignment02/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Assignment02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            //Person[] persons = new Person[3]
+            //{
+            //new Person { Name = "Ahmed", Age = 20 },
+            //new Person { Name = "Khalid", Age = 40 },
+            //new Person { Name = "Aliaa", Age = 30 }
+            //};
+            //Console.WriteLine("Details of persons:");
+            //foreach (var person in persons)Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+            #endregion
+
+            #region Q2
+            //Console.WriteLine("Enter Point 1:");
+            //Point p1 = new Point { X = double.Parse(Console.ReadLine()), Y = double.Parse(Console.ReadLine()) };
+            //Console.WriteLine("Enter Point 2:");
+            //Point p2 = new Point { X = double.Parse(Console.ReadLine()), Y = double.Parse(Console.ReadLine()) };
+            //double d = Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2));
+            //Console.WriteLine($"The distance between the points is: {d:F}");
+            #endregion
+
+            #region Q3
+            //Person[] persons = new Person[3];
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine($"Enter Name and Age for Person {i + 1}:");
+            //    persons[i].Name = Console.ReadLine();
+            //    persons[i].Age = int.Parse(Console.ReadLine());
+            //}
+            //Person old = persons[0];
+            //foreach (var person in persons)
+            //{
+            //    if (person.Age > old.Age)
+            //        old = person;
+            //}
+            //Console.WriteLine($"The oldest person is {old.Name}, Age: {old.Age}");
+            #endregion
+
+            #region Q4
+            //Rectangle rect = new Rectangle();
+            //Console.WriteLine("Enter width:");
+            //rect.Width = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter height:");
+            //rect.Height = double.Parse(Console.ReadLine());
+            //rect.Display();
+            #endregion
+        }
+    }
+}
---
+++ Assignment02/Rectangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    internal class Rectangle
+    {
+        private double width;
+        private double height;
+        public double Width
+        {
+            get { return width; }
+            set
+            {
+                if (value < 0)Console.WriteLine("Width cannot be negative.");
+                else width = value; 
+            }
+        }
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (value < 0)Console.WriteLine("Height cannot be negative.");
+                else height = value;
+            }
+        }
+        public double Area
+        {
+            get { return width * height; }
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Rectangle dimensions: Width = {width}, Height = {height}, Area = {Area}");
+        }
+    }
+}
